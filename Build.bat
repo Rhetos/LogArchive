b641a3485f3113c03621a18ac1145ce0d94ba7e2
Bojan Antolović <bojan.antolovic@gmail.com>
2018-05-29 12:22:47 +0200

    The concept LogReaderAdditionalSource is moved to the CommonConcepts package.

--- Build.bat
+++ Build.bat
@@ -2,18 +2,9 @@ SETLOCAL
 SET Version=1.0.0
 SET Prerelease=auto
 
-@SET Config=%1%
-@IF [%1] == [] SET Config=Debug
-
-CALL Tools\Build\FindVisualStudio.bat || GOTO Error0
 
 REM Updating the version of all projects.
 PowerShell -ExecutionPolicy ByPass .\Tools\Build\ChangeVersion.ps1 %Version% %Prerelease% || GOTO Error0
-
-REM NuGet Automatic Package Restore requires "NuGet.exe restore" to be executed before the command-line build.
-WHERE /Q NuGet.exe || ECHO ERROR: Please download the NuGet.exe command line tool. && GOTO Error0
-NuGet.exe restore Rhetos.LogArchive.sln -NonInteractive || GOTO Error0
-MSBuild.exe "Rhetos.LogArchive.sln" /target:rebuild /p:Configuration=%Config% /verbosity:minimal /fileLogger || GOTO Error0
 
 IF NOT EXIST Install md Install
 NuGet pack -OutputDirectory Install || GOTO Error0
