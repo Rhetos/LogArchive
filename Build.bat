34478a3854b2e99e24b5334a067636d1ddcc00fb
Bojan Antolović <bojan.antolovic@gmail.com>
2022-02-03 22:18:46 +0100

    Consistent build scripts between Rhetos packages.

--- Build.bat
+++ Build.bat
@@ -5,8 +5,10 @@ SET Prerelease=auto
 REM Updating the build version.
 PowerShell -ExecutionPolicy ByPass .\Tools\Build\ChangeVersion.ps1 %Version% %Prerelease% || GOTO Error0
 
-IF NOT EXIST Install\ MD Install
+IF NOT EXIST Install MD Install
 DEL /F /S /Q Install\* || GOTO Error0
+
+WHERE /Q NuGet.exe || ECHO ERROR: Please download the NuGet.exe command line tool. && GOTO Error0
 NuGet pack -OutputDirectory Install || GOTO Error0
 
 REM Updating the build version back to "dev" (internal development build), to avoid spamming git history with timestamped prerelease versions.
